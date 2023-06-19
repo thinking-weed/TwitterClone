7fd1a2f880385ca9d3a574241808f208b7c33be5
thinking-weed <133303560+thinking-weed@users.noreply.github.com>
2023-06-19 10:50:30 +0900

    バリデーションの追加

--- Controllers/sign-up.php
+++ Controllers/sign-up.php
@@ -19,6 +19,20 @@ if(isset($_POST['nickname']) && isset($_POST['name']) && isset($_POST['email']) 
         'email' => $_POST['email'],
         'password' => $_POST['password'],
     ];
+    //----------------------
+    //バリデーション（入力フォームの内容の妥当性をチェックするもの）
+    //-----------------------
+    //文字数制限（全ての入力項目に対して行う）
+    $length = mb_strlen($data['nickname']);
+    if($length < 1 || $length > 50){
+        $error_messages[] = 'ニックネームは1～50文字にしてください';
+    }
+    //メールアドレス
+    if(!filter_var($data['email'],FILTER_VALIDATE_EMAIL)){
+        $error_messages[] = 'メールアドレスが不正です。';
+    }
+
+
     //ユーザーを作成し成功すれば、
     if(createUser($data)){
         //ログイン画面に遷移
